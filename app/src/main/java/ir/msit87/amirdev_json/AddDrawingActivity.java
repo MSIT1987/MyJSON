2253909cfce4fce54a39662c80982671c457be2f
Mohammad Soltanmohammadi <msit26055@gmail.com>
2017-08-09 11:53:59 +0430

    There is no problem

---
+++ app/src/main/java/ir/msit87/amirdev_json/AddDrawingActivity.java
@@ -0,0 +1,8 @@
+package ir.msit87.amirdev_json;
+
+/**
+ * Created by MSIT on 8/7/2017.
+ */
+
+public class AddDrawingActivity {
+}
---
+++ app/src/main/java/ir/msit87/amirdev_json/MainActivity.java
@@ -0,0 +1,32 @@
+package ir.msit87.amirdev_json;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import eu.amirs.JSON;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String simpleJsonString = "{\"id\":1, \"name\":\"A green door\",  \"price\":12.5,  \"tags\":[\"home\",\"green\"]}";
+        JSON jsons = new JSON(simpleJsonString);
+
+        Double priec = jsons.key("price").doubleValue();
+        String firstIndex = jsons.key("tags").index(1).stringValue();
+
+        JSON generatedJsonObject = JSON.create(JSON.dic("someKey", "someValue",
+                "someArrayKey", JSON.array("first", 1, 2, JSON.dic("emptyArrayKey", JSON.array()))));
+
+        //generatedJsonObject.addEditWithKey("someArrayKey","someOtherValue");
+
+
+        generatedJsonObject.key("someArrayKey").removeWithIndex(1);
+
+        generatedJsonObject.addEditWithKey("someArrayKey", "someOtherValue");
+
+    }
+}
